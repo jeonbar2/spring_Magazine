44431e7b7c777f42bb040d35120a395639efd5a9
jeonho <jhkang1995@naver.com>
2022-02-21 21:49:56 +0900

    [BE] feat: User entity
    회원가입에 필요한 USER entity생성

---
+++ src/main/java/com/spring_magazine/SpringMagazineApplication.java
@@ -0,0 +1,13 @@
+package com.spring_magazine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMagazineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMagazineApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/spring_magazine/model/User.java
@@ -0,0 +1,35 @@
+package com.spring_magazine.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+
+@Getter // get 함수를 일괄적으로 만들어줍니다.
+@NoArgsConstructor(access = AccessLevel.PROTECTED)// 기본 생성자를 만들어줍니다.
+@Entity // DB 테이블 역할을 합니다.
+@Builder
+public class User {
+
+
+    // nullable: null 허용 여부
+// unique: 중복 허용 여부 (false 일때 중복 허용)
+    @Id
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String realName;
+
+
+    @Column(nullable = false)
+    private String password;
+
+
+
+    public User(String username,String realName, String password) {
+        this.username = username;
+        this.password = password;
+        this.realName = realName;
+    }
+}
